8f728512d0681b1bcbd713c0e6c36fdaa3b418ae
Casey Georgeson <caseys.georgeson@gmail.com>
2021-05-31 14:19:48 -0700

    split GLFW window functions to own struct

--- src/main.c
+++ src/main.c
@@ -1,16 +1,16 @@
-#include <glad/glad.h>
 
-#include <GLFW/glfw3.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
+#include "window.h"
 #include "renderer.h"
 #include "shader.h"
 #include "tetris.h"
 
-#include <math.h>
+
+#define FRAMERATE 144
 
 int
 main() {
@@ -20,45 +20,30 @@ main() {
 			game.playfield[x][y] = BLOCKCOLOR_EMPTY;
 		}
 	}
-
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	GLFWwindow* window = glfwCreateWindow(640, 480, "win", NULL, NULL);
-	glfwMakeContextCurrent(window);
-
-	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		return -1;
-	}
-
-	glViewport(0, 0, 640, 480);
-
-	struct Renderer ren = renderer_Init(640.0f, 480.0f);
-
-	float k = 0;
-	while(!glfwWindowShouldClose(window)) {
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		renderer_DrawQuad(
-		    ren,
-		    100,
-		    100,
-		    540,
-		    380,
-		    (Color_t){
-		        (sinf(glfwGetTime()) + 1.0f) * (255.0f / 2.0f) / 255.0f,
-		        (sinf(glfwGetTime() * 2) + 1.0f) * (255.0f / 2.0f) / 255.0f,
-		        (sinf(glfwGetTime() / 2) + 1.0f) * (255.0f / 2.0f) / 255.0f,
-		        1.0f});
-
-		glfwSwapBuffers(window);
+    struct Window win = window_Init(800, 600, "win");
+	struct Renderer ren = renderer_Init(win.win_x, win.win_y);
+    double start_time = glfwGetTime();
+	while(window_Active(win)) {
 		glfwPollEvents();
+        double acc_time = glfwGetTime() - start_time;
+        if(acc_time > 1.0f / FRAMERATE) {
+            renderer_DrawQuad(
+                ren,
+                100,
+                100,
+                540,
+                380,
+                (Color_t){
+                    (sinf(glfwGetTime()) + 1.0f) * (255.0f / 2.0f) / 255.0f,
+                    (sinf(glfwGetTime() * 2) + 1.0f) * (255.0f / 2.0f) / 255.0f,
+                    (sinf(glfwGetTime() / 2) + 1.0f) * (255.0f / 2.0f) / 255.0f,
+                    1.0f});
+        
+            window_Update(win);
+            start_time = glfwGetTime();
+        }
 	}
 
 	glfwTerminate();
-
 	return 0;
 }
---
+++ src/window.h
@@ -0,0 +1,15 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stdbool.h>
+
+struct Window {
+    GLFWwindow* window;
+    int win_x, win_y;
+};
+
+struct Window window_Init(int window_size_x, int window_size_y, const char* window_title);
+void window_Update(struct Window window_to_update);
+bool window_Active(struct Window window_to_check);
+#endif
